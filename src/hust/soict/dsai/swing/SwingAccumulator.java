1dd3fd52fefecc754cda5db212439881583cc1ad
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-29 07:12:19 +0700

    Lab05 initialize

---
+++ src/hust/soict/dsai/swing/SwingAccumulator.java
@@ -0,0 +1,9 @@
+package hust.soict.dsai.swing;
+
+import javax.swing.*;
+
+public class SwingAccumulator extends JFrame {
+    private JTextField tfInput;
+    private JTextField tfOutput;
+    private int sum = 0; //accumulated sum, init to 0
+}
