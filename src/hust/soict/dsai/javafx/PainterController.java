57326e878ab0fbbf8a298238d4c1548b41e50177
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-31 15:01:22 +0700

    JavaFX API

--- src/hust/soict/dsai/javafx/PainterController.java
+++ src/hust/soict/dsai/javafx/PainterController.java
@@ -1,4 +1,16 @@
 package hust.soict.dsai.javafx;
 
+import java.awt.event.ActionEvent;
+
 public class PainterController {
+    @FXML
+    private Pane drawingAreaPane;
+
+    @FXML
+    private clearButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    private
 }
