a401d44e46b09b1faee129365eec5cf32bae59ec
VinhTran <vinhthanhtran03102004@gmail.com>
2024-06-01 10:44:11 +0700

    JavaFX API

--- src/hust/soict/dsai/javafx/Painter.java
+++ src/hust/soict/dsai/javafx/Painter.java
@@ -1,4 +1,18 @@
 package hust.soict.dsai.javafx;
 
 public class Painter {
+    @Override
+    public void Start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().
+                getResource("/hust/soict/program/javafx/Painter.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Painter");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
