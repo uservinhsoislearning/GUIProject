8db67f59d91a068d64e01d1dc1fe9782a5a201c8
VinhTran <vinhthanhtran03102004@gmail.com>
2024-06-01 11:32:47 +0700

    JavaFX API

--- src/hust/soict/dsai/javafx/Painter.java
+++ src/hust/soict/dsai/javafx/Painter.java
@@ -11,7 +11,7 @@ public class Painter extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().
-                getResource("/Painter.fxml"));
+                getResource(""));
 
         Scene scene = new Scene(root);
         stage.setTitle("Painter");
