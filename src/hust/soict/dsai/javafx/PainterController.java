11e23d68a4f6f5639a5da058cd6499ea0236342f
VinhTran <vinhthanhtran03102004@gmail.com>
2024-06-01 10:53:29 +0700

    JavaFX API

--- src/hust/soict/dsai/javafx/PainterController.java
+++ src/hust/soict/dsai/javafx/PainterController.java
@@ -1,5 +1,10 @@
 package hust.soict.dsai.javafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
@@ -9,13 +14,13 @@ public class PainterController {
     private Pane drawingAreaPane;
 
     @FXML
-    private clearButtonPressed(ActionEvent event) {
+    void clearButtonPressed(ActionEvent event) {
         drawingAreaPane.getChildren().clear();
     }
 
     @FXML
-    private drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX()),
+    void drawingAreaMouseDragged(MouseEvent event) {
+        Circle newCircle = new Circle(event.getX(),
                 event.getY(), 4, Color.BLACK);
         drawingAreaPane.getChildren().add(newCircle);
     }
